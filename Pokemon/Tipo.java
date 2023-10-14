9a234dc753eb28368445da184fd085da9408af43
Roberstxx07 <132015061+Roberstxx07@users.noreply.github.com>
2023-10-13 21:48:28 -0600

    Create Tipo.java

---
+++ Pokemon/Tipo.java
@@ -0,0 +1,12 @@
+package pokemon.PoKemon;
+
+
+public enum Tipo {
+    ACERO, AGUA, BICHO, 
+    DRAGON, ELECTRICO, FANTASMA,
+    FUEGO, HADA, HIELO,
+    LUCHA, NORMAL, PLANTA,
+    PSIQUICO, ROCA, SINIESTRO,
+    TIERRA, VENENO, VOLADOR,
+    
+}
